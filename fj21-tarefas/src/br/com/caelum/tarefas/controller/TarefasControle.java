8a07efc7cb4b1c6999ee681a1d9fd2fd7434de6d
web6698 <web6698@caelum144-10>
2017-03-17 23:03:28 -0300

    ajax

--- fj21-tarefas/src/br/com/caelum/tarefas/controller/TarefasControle.java
+++ fj21-tarefas/src/br/com/caelum/tarefas/controller/TarefasControle.java
@@ -3,8 +3,10 @@ package br.com.caelum.tarefas.controller;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.com.caelum.tarefas.dao.JdbcTarefaDao;
 import br.com.caelum.tarefas.modelo.Tarefa;
@@ -27,5 +29,42 @@ public class TarefasControle {
 		JdbcTarefaDao dao = new JdbcTarefaDao();
 		dao.adiciona(tarefa);
 		return "tarefa/adicionada";
+	}
+	@RequestMapping("listaTarefas")
+	public String lista(Model model) {
+
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		model.addAttribute("tarefas", dao.lista());
+		return "tarefa/lista";
+	}
+	
+	@RequestMapping("removeTarefa")
+	public String remove(Tarefa tarefa) {
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		dao.remove(tarefa);
+		return "redirect:listaTarefas";
+	}
+	
+	@RequestMapping("mostraTarefa")
+	public String mostra(Long id, Model model) {
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		model.addAttribute("tarefa", dao.buscaPorId(id));
+		return "tarefa/mostra";
+	}
+	
+	@RequestMapping("alteraTarefa")
+	public String altera(Tarefa tarefa) {
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		dao.altera(tarefa);
+		return "redirect:listaTarefas";
+		
+	}
+	
+	@ResponseBody
+	@RequestMapping ("finalizaTarefa")
+	public void finaliza(long id) {
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		dao.finaliza(id);
+
 	}
 }
--- fj21-tarefas/src/br/com/caelum/tarefas/modelo/Tarefa.java
+++ fj21-tarefas/src/br/com/caelum/tarefas/modelo/Tarefa.java
@@ -5,6 +5,8 @@ import java.util.Calendar;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 
 public class Tarefa {
 
@@ -12,6 +14,8 @@ public class Tarefa {
 	@NotNull (message="Burro voce deve inserir algo")@Size(min=5)//melhor colocar empty
 	private String descricao;
 	private boolean finalizado;
+	
+	@DateTimeFormat (pattern="dd/MM/yyyy")
 	private Calendar dataFinalizacao;
 
 	public Long getId() {
@@ -41,6 +45,7 @@ public class Tarefa {
 	public Calendar getDataFinalizacao() {
 		return dataFinalizacao;
 	}
+
 
 	public void setDataFinalizacao(Calendar dataFinalizacao) {
 		this.dataFinalizacao = dataFinalizacao;
